87c3e75e0952bd27ef1f6069669c11b096aad010
belensosatosello <mb.sosatosello@gmail.com>
2020-04-06 16:00:14 -0300

    18.Publisher relationships

--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -1,9 +1,8 @@
 package guru.springframework.spring5webapp.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Publisher {
@@ -13,9 +12,43 @@ public class Publisher {
 	private Long id;
 
 	private String name;
-	private String address;
+	private String addressLine1;
+	private String city;
+	private String state;
+	private String zip;
+
+	@OneToMany
+	@JoinColumn(name = "publisher_id")
+	private Set<Book> books = new HashSet<>();
 
 	public Publisher() {
+	}
+
+	@Override
+	public String toString() {
+		return "Publisher{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				", addressLine1='" + addressLine1 + '\'' +
+				", city='" + city + '\'' +
+				", state='" + state + '\'' +
+				", zip='" + zip + '\'' +
+				'}';
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Publisher publisher = (Publisher) o;
+
+		return id != null ? id.equals(publisher.id) : publisher.id == null;
+	}
+
+	@Override
+	public int hashCode() {
+		return id != null ? id.hashCode() : 0;
 	}
 
 	public Long getId() {
@@ -34,26 +67,43 @@ public class Publisher {
 		this.name = name;
 	}
 
-	public String getAddress() {
-		return address;
+	public String getAddressLine1() {
+		return addressLine1;
 	}
 
-	public void setAddress(String address) {
-		this.address = address;
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
 	}
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-
-		Publisher publisher = (Publisher) o;
-
-		return id != null ? id.equals(publisher.id) : publisher.id == null;
+	public String getCity() {
+		return city;
 	}
 
-	@Override
-	public int hashCode() {
-		return id != null ? id.hashCode() : 0;
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public Set<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(Set<Book> books) {
+		this.books = books;
 	}
 }
